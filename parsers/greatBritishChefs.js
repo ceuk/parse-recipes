c52a006e0d5f470743dfe4e572e9137dde4864fd
ceuk <ceuk.dev@gmail.com>
2020-06-28 22:38:57 +0100

    improve gbc parser

--- parsers/greatBritishChefs.js
+++ parsers/greatBritishChefs.js
@@ -8,10 +8,14 @@ module.exports = $ => {
   const imageUrl = $('#head-media').attr('src')
   const description = $('.RecipeAbstract__Abstract').text().trim()
   const method = $('.MethodList__StepText').toArray().map(step => $(step).text().trim())
-  const ingredients = $('.IngredientsList__GroupTitleItem').toArray().map(section => ({
+  const groups = $('.IngredientsList__GroupTitleItem').toArray()
+  const ingredients = groups.length ? groups.map(section => ({
     title: $(section).find('h4').text().trim(),
-    items: $(section).next('ul').children('li').toArray().map(ingredient => parse($(ingredient).text()))
-  }))
+    items: $(section).next('ul').children('li').toArray().map(ingredient => parse($(ingredient).text().replace(/\s\s+/g, ' ').trim()))
+  })) : [{
+    title: '',
+    items: $('.IngredientsList__IngredientItem').toArray().map(ingredient => parse($(ingredient).text().replace(/\s\s+/g, ' ').trim()))
+  }]
 
   return {
     title,
