df5b083fdad7206769114f0b9e80e052d06480f7
ceuk <ceuk.dev@gmail.com>
2020-07-06 10:41:27 +0100

    update parser response format

--- handler.js
+++ handler.js
@@ -1,18 +1,21 @@
 const Parser = require('./lib/Parser')
 
-export const parse = async event => {
+export const parse = async (event, context, cb) => {
   try {
-    const parser = new Parser(event)
+    const parser = new Parser(event.body)
     const recipe = await parser.parse()
     return {
       statusCode: 200,
-      body: recipe
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(recipe)
     }
   } catch (err) {
-    console.err(err)
+    console.error(err)
+    cb(err)
     return {
       statusCode: 500,
-      body: err.toString()
+      headers: { 'Content-Type': 'application/json' },
+      body: { error: err.toString() }
     }
   }
 }
